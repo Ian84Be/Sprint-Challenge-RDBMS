0c15fea723a01021b2cb602f4a526ee2f45dadb4
Ian Be <ian84be@gmail.com>
2019-03-29 12:47:40 -0500

    STRETCH: crud action-router.js & project-router.js

--- data/helpers/project-router.js
+++ data/helpers/project-router.js
@@ -43,4 +43,41 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req,res) => {
+    const {id} = req.params;
+    if (!req.body) {
+        res.status(400).json({error:'Please provide a description / name'});
+    }
+    else {
+        try {
+            const changes = await db('projects').where({id}).update(req.body);
+            if (changes) {
+                const result = await db('projects').where({id}).first();
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({error:"id not found"});
+            }
+        }
+        catch(err) {
+            res.status(500).json({error:"Failed to update resource."});
+        }
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const removed = await db('projects').where({id}).del();
+        if (removed) {
+            res.status(200).json({message:"Resource deleted."});
+        }
+        else {
+            res.status(404).json({error:"Cannot find action with that ID."});
+        }
+    }
+    catch(err) {
+        res.status(500).json({error:"Failed to delete resource."});
+    }
+});
+
 module.exports = router;
